b1175240e29962ac76d4af799458323d15b39a62
NikkoleAshleigh <nikkole.a.spurgeon@gmail.com>
2020-12-18 07:19:28 -0700

    packages/images/start of folder structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
-import React from "react"
+import React, { Component } from "react";
+// import Carousel from "../../components/Carousel";
+// import WhatWeDo from "../../components/WhatWeDo";
+// import WhoWeAre from "../../components/WhoWeAre";
+// import Information from "../../components/Information";
+import ReactGA from 'react-ga';
+export default class Home extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    ReactGA.initialize('UA-169087465-1')
+    ReactGA.pageview("/"); 
+  }
 
-export default function Home() {
-  return <div>Hello world!</div>
+  render() {
+    return (
+      <div className="container-fluid n-p p-0">
+        {/* <Carousel /> */}
+        <div className="container-fluid n-p p-0">
+          {/* <WhoWeAre /> */}
+          {/* <WhatWeDo /> */}
+        </div>
+        <div className="container-fluid n-p p-0">
+          {/* <Information /> */}
+        </div>
+      </div>
+    );
+  }
 }
